4f1b42c509bed7a5b5d338bdbe7f853bdde9f419
Alina Kathayat <ariasushi01@gmail.com>
2024-11-26 06:54:57 +0545

    Update try1.py

--- try1.py
+++ try1.py
@@ -1,59 +1,29 @@
 import turtle
 # Set up the screen
-screen =
-turtle.Screen()
-screen.
-bgcolor("white")
+screen =turtle.Screen()
+screen.bgcolor("white")
 
 # Create turtle object
 
-
-
-flower = turtle.
-Turtle()
-flower.
-speed(0)
-flower.
-   color("purple")
-
-
+flower = turtle.Turtle()
+flower.speed(0)
+flower.color("purple")
 
 # Function to draw a petal
-
-
 def draw_petal():
+ flower.circle(50, 60)
+flower.left(120)
+flower.circle(50, 60)
+flower.left(120)
 
-
-    
-    flower.
-    
-    
-    circle(50, 60)
-    flower.
-    
-    
-    left(120)
-    flower.
-    
-    circle(50, 60)
-    
-    flower.
-    
-    
-    left(120)
 # Draw flower
-
-
-flower.
-penup()
+flower.penup()
 flower.goto(0, -50)
 flower.pendown()
-for _ in range(6):
 
-    
-    draw_petal()
-    flower.
-    right(60)
+for _ in range(6):
+  draw_petal()
+  flower.right(60)
 
 # Finish
 flower.hideturtle()
