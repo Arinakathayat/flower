f510dccc8b9dd9afa9f90c809c6a12bef477c83b
Alina Kathayat <ariasushi01@gmail.com>
2024-11-29 20:29:00 +0545

    Create random.c

---
+++ random.c
@@ -0,0 +1,15 @@
+#include <iostream.h>
+#include <fstream.h>
+using namespace std;
+class student 
+{
+char name[30];
+int roll ;
+public: void read data ()
+cout<<"enter name and roll";
+cin>>name>> roll;
+}
+void show_data
+{cout<<"name and roll="<<name<<roll;
+}
+};
